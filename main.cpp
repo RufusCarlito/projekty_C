166085f506f4ce38f1ff27162c511837f233673e
RufusCarlito <66885342+RufusCarlito@users.noreply.github.com>
2021-01-19 21:03:28 +0100

    krystian

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+int test, s;
+int f(int s)
+{
+    if(s==1) return o;
+    else return f(n-1)/2;
+}
+
+
 int main() {
-    std::cout << "Test!" << std::endl;
+
+    cin>>test;
+    for (int i=0; i<=test; i++)    {
+        cin>>s;
+        if((s%2==0) || (s==0))
+        {
+
+        }else cout <<"nieparzyste" <<endl;
+    }
+
+
     return 0;
 }
